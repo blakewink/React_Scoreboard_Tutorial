e086639f09dfeb7fb39a5cb9361c8ca3942852a1
Joshua Wink <blakewink@gmail.com>
2022-02-24 14:07:32 -0500

    commit on 'master'

---
+++ app.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+const title = React.createElement(
+    'h1',
+    { id: 'main-title', title:'This is a title.'},
+    'My First React Element!'
+);
+
+ReactDOM.render(
+    title, 
+    document.getElementById('root')
+);
+//testing
