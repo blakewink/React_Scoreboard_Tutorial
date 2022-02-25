074d4fcaa756b61accfab36c56e31fbdf6c1ce0f
Joshua Wink <blakewink@gmail.com>
2022-02-24 19:55:30 -0500

    Added state changes

--- app.js
+++ app.js
@@ -36,20 +36,39 @@ const Player = (props) => {
             <span className="player-name">
                 {props.name}
             </span>
-            <Counter score={props.score}/>
+            <Counter />
         </div>
     );
 };
 
 
-const Counter = (props) => {
-    return(
-        <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <span className="counter-score">{props.score}</span>
-                <button className="counter-action increment"> + </button>
-        </div>
-    );
+class Counter extends React.Component {
+
+    state = {
+        score: 0
+    };
+    
+    incrementScore = () => {
+        this.setState({
+            score: this.state.score + 1
+        });
+    };
+
+    decrementScore = () => {
+        this.setState({
+            score: this.state.score - 1
+        });
+    };
+
+    render(){
+        return(
+            <div className="counter">
+                    <button className="counter-action decrement" onClick={this.decrementScore}> - </button>
+                    <span className="counter-score">{this.state.score}</span>
+                    <button className="counter-action increment" onClick={this.incrementScore}> + </button>
+            </div>
+        );   
+    }
 };
 
 
@@ -61,7 +80,6 @@ const App = (props) =>{
                 props.initialPlayers.map( player => 
                     <Player 
                     name={player.name}
-                    score={player.score}
                     key={player.id.toString()}    
                     />
                 )
